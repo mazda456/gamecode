8e7d3b4c1d3598f3ed24e2ca93bdaf76a161cf1f
mazda456 <102490423+mazda456@users.noreply.github.com>
2023-09-19 12:53:20 +1200

    Add files via upload

--- DTC Game 2023/game.js
+++ DTC Game 2023/game.js
@@ -1,27 +1,54 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
+const character = document.getElementById("character");
+const moveAmount = 10;
 
-var timeoutHandle;
-function countdown(minutes, seconds) {
-    function tick() {
-        var counter = document.getElementById("timer");
-        counter.innerHTML =
-            minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
-        seconds--;
-        if (seconds >= 0) {
-            timeoutHandle = setTimeout(tick, 1000);
-        } else {
-            if (minutes >= 1) {
-                // countdown(mins-1);   never reach “00″ issue solved:Contributed by Victor Streithorst
-                setTimeout(function () {
-                    countdown(minutes - 1, 59);
-                }, 1000);
-            }
-        }
+// Function to move the character
+function moveCharacter(direction) {
+    switch (direction) {
+        case "left":
+            characterX -= moveAmount;
+            break;
+        case "right":
+            characterX += moveAmount;
+            break;
+        case "up":
+            characterY -= moveAmount;
+            break;
+        case "down":
+            characterY += moveAmount;
+            break;
     }
-    tick();
+    character.style.left = characterX + "px";
+    character.style.top = characterY + "px";
 }
 
-countdown(1, 0);
+// Function to set the initial position of the character
+function setInitialPosition(x, y) {
+    characterX = x;
+    characterY = y;
+    character.style.left = characterX + "px";
+    character.style.top = characterY + "px";
+}
 
+// Set the initial position (you can change these values)
+setInitialPosition(375, 320);
+
+// Event listeners for arrow keys
+document.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "ArrowLeft":
+            moveCharacter("left");
+            break;
+        case "ArrowRight":
+            moveCharacter("right");
+            break;
+        case "ArrowUp":
+            moveCharacter("up");
+            break;
+        case "ArrowDown":
+            moveCharacter("down");
+            break;
+    }
+});
